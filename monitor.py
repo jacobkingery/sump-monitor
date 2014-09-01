24097dd51fef88bac69a7f53b67bf893ea85ed25
Jacob Kingery <jacob.kingery@students.olin.edu>
2014-09-01 16:12:00 -0500

    added streaming and SMS warning

--- monitor.py
+++ monitor.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 import plotly.plotly as py
 from plotly.graph_objs import Scatter, Data
 import datetime as dt
-import time, subprocess, traceback
+import time, subprocess
 
 def pinSetup(rP, wPs):
     GPIO.setmode(GPIO.BOARD)
@@ -23,6 +23,18 @@ def getLevel(rP, wPs):
         if not i:
             return 0
 
+def streamData(id, ts, lvl):
+    data = {'x':ts, 'y':lvl}
+
+    # Try streaming to server; ignore if it fails
+    try:
+        s = py.Stream(id)
+        s.open()
+        s.write(data)
+        s.close()
+    except:
+        pass
+
 def logData(bl):
     trace = Scatter(
         x = bl['x'],
@@ -38,13 +50,43 @@ def logData(bl):
         print len(bl['x'])
         return bl
 
+def sendSMS(no, lvl, last):
+    now = dt.datetime.now()
+    delta = now - last
+
+    # If it has been enough time since the last SMS, try to send the warning up to 5 times
+    if delta.total_seconds() > 7200:
+        atmpt = 5
+        while atmpt:
+            try:
+                subprocess.call(['curl', 'http://textbelt.com/text',
+                    '-d', 'number={0}'.format(no),
+                    '-d', "message=Water in sump hole has reached level {0}.".format(lvl)
+                ])
+                return now
+            except:
+                atmpt -= 1
+                time.sleep(10)
+    return last
+
 try:
     readPin = 7
     writePins = [11, 12, 13, 15, 16, 18, 22]    # From lowest to highest (physically)
     pinSetup(readPin, writePins)
 
-    with open('phonenumber.txt') as f:
-        phoneNo = f.readline().strip()
+    try:
+        with open('phonenumber.txt') as f:
+            phoneNo = f.readline().strip()
+    except:
+        phoneNo = 0
+
+    lastMsg = dt.datetime.min
+
+    try:
+        with open('streamID.txt') as f:
+            streamID = f.readline().strip()
+    except:
+        streamID = 0
 
     backlog = {'x':[], 'y':[]}
 
@@ -52,7 +94,12 @@ try:
         level = getLevel(readPin, writePins)
         timestamp = dt.datetime.now()
 
+        if level >= 6 and phoneNo:
+            lastMsg = sendSMS(phoneNo, level, lastMsg)
+
         # send every data point to stream
+        if streamID:
+            streamData(streamID, timestamp, level)
 
         # send every <x> data points to archive
         backlog['x'].append(timestamp)
@@ -61,11 +108,10 @@ try:
         if len(backlog['x']) > 5:
             backlog = logData(backlog)
 
-        time.sleep(60)
+        time.sleep(5)
 
 except:
     GPIO.cleanup()
-    traceback.print_exc()
     sent = 0
     while not sent:
         try:
